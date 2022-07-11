528b1301bc3fbdcb4bed452657a245ce6f3d727b
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-11 13:56:32 -0400

    :construction: Work in fix error

--- lighthouserc-ssr.js
+++ lighthouserc-ssr.js
@@ -11,7 +11,7 @@ module.exports = {
         "categories:performance": ["error", { minScore: 0.75 }],
         "categories:accessibility": ["error", { minScore: 0.8 }],
         "categories:best-practices": ["error", { minScore: 0.9 }],
-        "categories:seo": ["error", { minScore: 0.8 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
       },
     },
     upload: {
