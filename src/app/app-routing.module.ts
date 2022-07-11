94746c540121e872945a3f70e71fe74a56715e8a
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-11 10:19:50 -0400

    :construction: work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    pathMatch: 'full'
   }
 ];
 
