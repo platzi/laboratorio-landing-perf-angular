3b14f8c2aa570549e30c7e9cea52dd2b3e43fd9c
Kevin <kevinfiorentino.1294@gmail.com>
2023-01-13 20:43:55 -0300

    Fix modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +11,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    SharedModule,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
-import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
@@ -9,7 +8,6 @@ import { AppComponent } from './app.component';
   imports: [
     AppModule,
     ServerModule,
-    SharedModule,
   ],
   bootstrap: [AppComponent],
 })
